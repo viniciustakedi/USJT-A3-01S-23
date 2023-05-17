f1cd45b89a0cbc6371b321d6f17d54b7a91031e3
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-05-16 22:53:44 -0300

    Change to upstash team cluster

--- apps/microservice/user/src/cluster/kafka/index.ts
+++ apps/microservice/user/src/cluster/kafka/index.ts
@@ -1,11 +1,11 @@
 import { Kafka } from 'kafkajs'
 
 const kafka = new Kafka({
-  brokers: ['climbing-gator-14806-us1-kafka.upstash.io:9092'],
+  brokers: ['golden-werewolf-10765-us1-kafka.upstash.io:9092'],
   sasl: {
     mechanism: 'scram-sha-256',
-    username: 'Y2xpbWJpbmctZ2F0b3ItMTQ4MDYk60TMw6yNtCWmDDqsTRyd20xGvnshA_xWMcQ',
-    password: 'bc52db987f144d93857f2414db0f4001'
+    username: 'Z29sZGVuLXdlcmV3b2xmLTEwNzY1JDL-c5lusHLBRXjBklEHWLbaBLllzhiFCLs',
+    password: '7acdd74bb3c4464abee224710ffb964e'
   },
   ssl: true
 })
