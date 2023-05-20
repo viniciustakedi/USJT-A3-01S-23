24dde98c844ed965b45d6cc65158680ef23510ff
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-05-20 18:03:15 -0300

    Add missing cors method

--- apps/microservice/tracks/src/server.ts
+++ apps/microservice/tracks/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { router } from './modules/routes'
 import cors from 'cors'
+import './providers/kafka/consumers'
 
 const app = express()
+const port = 5001
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(5000, () => console.log('TRACKS SERVER RUNNING ON PORT 5000'))
+app.listen(port, () => console.log(`TRACKS SERVER RUNNING ON PORT ${port}`))
