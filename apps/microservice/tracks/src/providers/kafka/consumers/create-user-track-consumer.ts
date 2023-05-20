e3b8ca604fbe0faac60baf6ad3df17df648c699f
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-05-20 04:05:34 -0300

    Add track service to message consumer

--- apps/microservice/tracks/src/providers/kafka/consumers/create-user-track-consumer.ts
+++ apps/microservice/tracks/src/providers/kafka/consumers/create-user-track-consumer.ts
@@ -1,6 +1,9 @@
 import { prismaClient } from '../../../database/prisma-client'
+import { CreateTracksService } from '../../../modules/create-tracks/create-tracks-service'
 import { CreateUserTracksParams } from '../../../types/CreateUserTracksParams'
 import { kafkaConsumer } from '../kafka.consumer'
+
+const service = new CreateTracksService()
 
 export async function createUserTrackConsumer() {
   console.log("TRACKS CONSUMER LISTENING")
@@ -9,21 +12,11 @@ export async function createUserTrackConsumer() {
     eachMessage: async ({ message }) => {
       const messageToString = message.value!.toString()
       const messageToJSON = JSON.parse(messageToString) as CreateUserTracksParams
-      console.log(messageToString)
 
-      await prismaClient.$transaction(
-        messageToJSON.tracks.map(track => prismaClient.track.create({
-          data: {
-            name: track.name,
-            duration: track.duration,
-            albumName: track.albumName,
-            imageUrl: track.imageUrl,
-            spotifyId: track.spotifyId,
-            uri: track.uri,
-            userId: messageToJSON.userId
-          }
-        }))
-      )
+      console.log(messageToJSON)
+
+      await service.execute(messageToJSON)
+      console.log("TRACKS CONSUMER EXECUTED")
     }
   })
 }
