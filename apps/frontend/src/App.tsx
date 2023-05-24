326c71aa9cd81a187db7d342af80ccee5af856df
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-05-24 14:56:23 -0300

    Add material cache provider

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,14 +2,22 @@ import { RouterProvider } from 'react-router-dom'
 import CustomThemeProvider from './components/CustomThemeProvider/CustomThemeProvider'
 import { router } from './router'
 import { CssBaseline } from '@mui/material'
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
 function App() {
+  const cache = createCache({
+    key: 'css',
+    prepend: true,
+  });
 
   return (
-    <CustomThemeProvider>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </CustomThemeProvider>
+    <CacheProvider value={cache}>
+      <CustomThemeProvider>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </CustomThemeProvider>
+    </CacheProvider>
   )
 }
 
