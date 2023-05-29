9943afeb47d9cfbf44b11deb48c24af18ce2e634
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-05-29 18:31:17 -0300

    Update port env name

--- apps/microservice/users/src/server.ts
+++ apps/microservice/users/src/server.ts
@@ -3,7 +3,7 @@ import { router } from './modules/routes'
 import cors from 'cors'
 
 const app = express()
-const port =  process.env.API_PORT || 5000
+const port =  process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
